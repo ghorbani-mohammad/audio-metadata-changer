2c4c507dc0df72beeec62883bdb38e0f056dd97b
Mohammad <aali361@gmail.com>
2022-07-13 07:06:15 +0430

    feat: init project

---
+++ code.py
@@ -0,0 +1,13 @@
+import os
+import eyed3
+
+path = "/mnt/c/Users/Mohammad/Desktop/all-ears-english/english-fluency/"
+path = input("Please enter path of mp3 directory: ")
+files = os.listdir(path)
+eyed3.log.setLevel("ERROR")
+
+
+for file in files:
+    ext = file.split(".")[-1]
+    audio_file = eyed3.load(path + file)
+    os.rename(path + file, path + f"{audio_file.tag.title}.{ext}")
